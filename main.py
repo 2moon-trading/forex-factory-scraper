e8c07d7b603316d094d4e1ed1c023fe28d91c38c
Santiago Galeano Grandeth <santiago.galeano@infiny.com.co>
2025-08-10 13:09:56 -0500

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from dateutil.tz import gettz # type: ignore
 from src.incremental import scrape_incremental
 
 
-start = '2025-07-27'
-cycles = 2
+start = '2025-08-10'
+cycles = 1
 timezone = 'Asia/Tehran'
 
 
